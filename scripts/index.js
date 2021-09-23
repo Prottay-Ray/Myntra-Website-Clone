10f4d37a67aa700dd0762167d429d0aefcd08e88
Prottay Ray <prottayray@gmail.com>
2021-09-23 22:28:47 +0530

    searchbar updating

--- scripts/index.js
+++ scripts/index.js
@@ -96,7 +96,7 @@ function vanish() {
 }
 
 
-let debounceTimer;
+let debounceTimer, time;
 let searchList = document.getElementById('searchlist');
 let searchWindow = document.getElementById('searchresult');
 
@@ -109,29 +109,49 @@ async function autoComplete() {
     let count = 0;
     clearTimeout(debounceTimer);
     debounceTimer = setTimeout(async () => {
-        let a = await fetch(`https://kohls.p.rapidapi.com/auto-complete?query=${val}`, {
+        let a = await fetch(`https://asos2.p.rapidapi.com/v2/auto-complete?q=${val}&store=US&country=US&currency=USD&sizeSchema=US&lang=en-US`, {
             "method": "GET",
             "headers": {
-                "x-rapidapi-host": "kohls.p.rapidapi.com",
+                "x-rapidapi-host": "asos2.p.rapidapi.com",
                 "x-rapidapi-key": "e41ac5f444mshb544d722349d34cp12a1c9jsn14405c0bb667"
             }
         });
         let b = await a.json();
-        //console.log("Response: ", b.Suggestions);
         searchWindow.style.display = "block";
         searchList.innerHTML = null;
-
-        b.Suggestions.forEach(element => {
-            //console.log("Response: ", element[0]);
+        for (let {searchTerm} of b.suggestionGroups[0].suggestions) {
             let li = document.createElement('li');
-            li.innerText = element[0];
+            li.innerText = searchTerm;
             if (count == 0) {
                 li.focus();
                 count = 1;
             }
+            li.addEventListener('click', () => {
+                alert("hareKrishna")
+                localStorage.setItem('searchItem', JSON.stringify({item: searchTerm}));
+                window.location.href = "search.htm";
+            });
             searchList.appendChild(li);
-        });
+        }
+        clearTimeout(time);
+        time = setTimeout(() => {
+            searchWindow.style.display = "none";
+            searchList.innerHTML = null;
+        }, 5000);
+        
     }, 1000);
 }
+
+searchWindow.addEventListener('mouseover', () => {
+    clearTimeout(time);
+})
+searchWindow.addEventListener('mouseleave', () => {
+    clearTimeout(time);
+
+    time = setTimeout(() => {
+        searchList.innerHTML = null;
+        searchWindow.style.display = "none";
+    }, 2000);
+})
 
 search.addEventListener('input', autoComplete);
--- scripts/login.js
+++ scripts/login.js
@@ -52,3 +52,63 @@ function submitPhone(e) {
 }
 
 cont.addEventListener('click', submitPhone);
+
+let search = document.getElementById('search');
+let searchbox = document.getElementById('searchbox');
+
+search.onfocus = () => {
+    searchbox.style.backgroundColor = "white"
+    searchbox.style.border = "1px solid lightgray"
+}
+search.onblur = () => {
+    searchbox.style.backgroundColor = "#F5F5F6"
+    searchbox.style.border = "none"
+}
+
+let debounceTimer, time;
+let searchList = document.getElementById('searchlist');
+let searchWindow = document.getElementById('searchresult');
+
+async function autoComplete() {
+    let val = search.value;
+    // alert('Hare Krishna')
+    if (val.length < 3) {
+        return;
+    }
+    let count = 0;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(async () => {
+        let a = await fetch(`https://asos2.p.rapidapi.com/v2/auto-complete?q=${val}&store=US&country=US&currency=USD&sizeSchema=US&lang=en-US`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "asos2.p.rapidapi.com",
+                "x-rapidapi-key": "e41ac5f444mshb544d722349d34cp12a1c9jsn14405c0bb667"
+            }
+        });
+        let b = await a.json();
+        searchWindow.style.display = "block";
+        searchList.innerHTML = null;
+        for (let {searchTerm} of b.suggestionGroups[0].suggestions) {
+            let li = document.createElement('li');
+            li.innerText = searchTerm;
+            if (count == 0) {
+                li.focus();
+                count = 1;
+            }
+            li.addEventListener('click', () => {
+                alert("hareKrishna")
+                localStorage.setItem('searchItem', JSON.stringify({item: searchTerm}));
+                window.location.href = "search.htm";
+            });
+            searchList.appendChild(li);
+        }
+        clearTimeout(time);
+        time = setTimeout(() => {
+            searchWindow.style.display = "none";
+            searchList.innerHTML = null;
+        }, 5000);
+        
+    }, 1000);
+}
+
+search.addEventListener('input', autoComplete);
